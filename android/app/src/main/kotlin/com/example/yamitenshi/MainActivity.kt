7225ff18e61e95e31a2f20924f81c46a610f8655
YamiTenshii <yamitenshi2024@hotmail.com>
2025-07-11 16:30:49 -0500

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/yamitenshi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yamitenshi/MainActivity.kt
@@ -1,6 +1,18 @@
-package com.example.yamitenshi
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.example.yamitenshi">
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+    <application
+        android:label="YamiTenshi"
+        android:icon="@mipmap/ic_launcher">
+        <activity
+            android:name="com.example.yamitenshi.MainActivity"
+            android:exported="true"
+            android:launchMode="singleTop"
+            android:theme="@style/Theme.AppCompat.Light.DarkActionBar">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+    </application>
+</manifest>
